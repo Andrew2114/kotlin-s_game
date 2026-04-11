db2cd3b9021f0f65d7d4cf69e290d6f349f17102
Andrew <taradeevandrej15@gmail.com>
2026-04-11 13:28:52 +0300

    Fix typos in build.gradle.kts: mavenCentral, implementation, isDirectory

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,32 @@
 plugins {
-kotlin("jvm") version "1.9.22"
-application
+    kotlin("jvm") version "1.9.22"
+    application
 }
 
 group = "com.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-mavenCantral()
+    mavenCentral()
 }
 
 dependencies {
-implemention("org.jetbrains.kotlin:kotlin-stdlib")
-testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    testImplementation(kotlin("test"))
 }
 
 application {
-mainClass.set("Main.kt")
+    mainClass.set("MainKt")
 }
 
 tasks.test {
-useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.jar {
-manifest {
-attributes["Main-Class"] = "MainKt"
-}
-from(configurations.runtimeClasspath.get().map { if (it.isDirictory) it else zipTree(it) })
-duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
