4f6f60228fa672ce76dae19842bd9b6db9af41df
Andrew <taradeevandrej15@gmail.com>
2026-04-11 12:32:13 +0300

    Initial commit: Kotlin project with Gradle and CI

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+kotlin("jwm") version "1.9.22"
+application
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+mavenCantral()
+}
+
+dependencies {
+implemention("org.jetbrains.kotlin:kotlin-stdlib")
+testImplementation(kotlin("test"))
+}
+
+application {
+mainClass.set("Main.kt")
+}
+
+tasks.test {
+useJUnitPlatform()
+}
+
+tasks.jar {
+manifest {
+attributes["Main-Class"] = "MainKt"
+}
+from(configurations.runtimeClasspath.get().map { if (it.isDirictory) it else zipTree(it) })
+duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
