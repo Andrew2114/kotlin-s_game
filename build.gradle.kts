266bcda741dbace8af07cdb96a4cc7a9c45a4140
Andrew <taradeevandrej15@gmail.com>
2026-05-18 03:11:34 +0300

    Add clean versions of files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,4 @@
-plugins {
-    kotlin("jvm") version "1.9.22"
-    application
-    id("org.openjfx.javafxplugin") version "0.0.13"
-}
-
-group = "com.example"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-javafx {
-    version = "20"
-    modules = listOf("javafx.controls", "javafx.fxml")
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("org.xerial:sqlite-jdbc:3.46.1.0")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+mplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.0")
 }
