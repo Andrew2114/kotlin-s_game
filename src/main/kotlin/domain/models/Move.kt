302c98682807fc1a49039557c02b5c035d6c05ff
Andrew <taradeevandrej15@gmail.com>
2026-05-22 18:37:03 +0300

    update file Move

--- src/main/kotlin/domain/models/Move.kt
+++ src/main/kotlin/domain/models/Move.kt
@@ -1,9 +1,7 @@
 package domain.models
-import java.time.LocalDateTime
 
 data class Move (
     val moveNumber: Int,
     val guess: Combination,
     val feedback: Feedback,
-    val timestamp: Long = System.currentTimeMillis()
 )
