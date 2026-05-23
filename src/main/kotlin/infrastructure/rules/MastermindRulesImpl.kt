524ba9fa5c0504d2d877348c9ade62b478d96af0
Andrew_Taradeev <156700173+Andrew2114@users.noreply.github.com>
2026-05-23 03:50:26 +0300

    Update MastermindRulesImpl.kt

--- src/main/kotlin/infrastructure/rules/MastermindRulesImpl.kt
+++ src/main/kotlin/infrastructure/rules/MastermindRulesImpl.kt
@@ -4,41 +4,39 @@ import domain.models.*
 import domain.rules.MastermindRules
 
 class MastermindRulesImpl : MastermindRules {
+
     override fun calculateFeedback(secret: Combination, guess: Combination): Feedback {
-        var blackCnt = 0
-        var whiteCnt = 0
+        var blackPins = 0
+        var whitePins = 0
 
         val secretList = secret.colors.toMutableList()
         val guessList = guess.colors.toMutableList()
 
-        // Сначала считаем чёрные пины (точные совпадения)
-        val indicesToRemove = mutableListOf<Int>()
+        val toRemove = mutableListOf<Int>()
         for (i in secretList.indices) {
             if (secretList[i] == guessList[i]) {
-                blackCnt++
-                indicesToRemove.add(i)
+                blackPins++
+                toRemove.add(i)
             }
         }
 
-        // Удаляем совпавшие элементы (с конца, чтобы не сбивать индексы)
-        for (i in indicesToRemove.sortedDescending()) {
+        for (i in toRemove.sortedDescending()) {
             secretList.removeAt(i)
             guessList.removeAt(i)
         }
 
-        // Затем считаем белые пины (цвет есть, но позиция другая)
         val secretRemaining = secretList.toMutableList()
         val guessRemaining = guessList.toMutableList()
 
         for (color in guessRemaining) {
             val index = secretRemaining.indexOf(color)
             if (index != -1) {
-                whiteCnt++
+                whitePins++
                 secretRemaining.removeAt(index)
             }
         }
 
-        return Feedback(blackCnt, whiteCnt)
+        return Feedback(blackPins, whitePins)
     }
 
     override fun validateGuess(guess: Combination): Boolean {
